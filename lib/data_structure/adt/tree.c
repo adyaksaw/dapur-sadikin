af8c20cd7135680220e486143902d6e1cbbec281
Mohammad Ridwan Hady Arifin <ridwanhady31@gmail.com>
2018-11-18 20:04:53 +0700

    Added Tree (Kurang 1 Prosedur)

--- lib/data_structure/adt/tree.c
+++ lib/data_structure/adt/tree.c
@@ -1,12 +1,9 @@
 /* ADT Pohon Biner */
 /* Implementasi dengan menggunakan pointer */
-/* Penamaan type TreeType, type addrNode, dan beberapa fungsi disesuikan 
+/* Penamaan type infotype, type addrNode, dan beberapa fungsi disesuikan 
    karena melibatkan modul list rekursif. */
 
-#ifndef _BINTREE_H_
-#define _BINTREE_H_
-
-/* Modul lain yang digunakan : */
+/* *** PROTOTYPE *** */
 #include "listrek.h"
 #include "boolean.h"
 #include "tree.h"
@@ -14,61 +11,86 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* #define Nil NULL */ /* konstanta Nil sesuai pada modul listrek */
-
-/* *** Definisi Type Pohon Biner *** */
-typedef char TreeType[20]; 
-typedef struct tNode *addrNode;
-typedef struct tNode
-{
-  TreeType info;
-  addrNode left;
-  addrNode right;
-} Node;
-
-/* Definisi PohonBiner : */
-/* Pohon Biner kosong : P = Nil */
-typedef addrNode BinTree;
-
-/* *** PROTOTYPE *** */
-
-/* *** Selektor *** */
-#define Akar(P) (P)->info
-#define Left(P) (P)->left
-#define Right(P) (P)->right
-
-void BuildTree(Bintree P);
+/* *** Konstruktor *** */
+void BuildTree(Bintree P)
 /* Membentuk sebuah pohon biner P dari pita karakter yang dibaca */
 /* I.S. Pita berisi "konstanta" pohon dalam bentuk prefix. Memori pasti cukup, alokasi pasti  berhasil. */
 /* F.S. P dibentuk dari ekspresi dalam  pita */
+{
+	//Kamus Lokal
+	char Recipe[256];
+	int i;
+	//Algoritma
+	ADV();
+	if (CC==')')
+		(*P)=Nil;
+	else {
+		AlokNode(P);
+		for (i=0; i<256; i++)
+			Num[i] = 0;
+		i = 0;
+		while ((CC !='(')&&(CC != ')')) {
+			Recipe[i] = CC;
+			ADV();
+			i++;
+		}
+    Recipe[i] = '\0';
+		Akar(*P) = Recipe;
+		BuildTree(&Left(*P));
+		BuildTree(&Right(*P));
+	}
+	ADV();
+}
 /* Manajemen Memory */
-
-addrNode AlokNode(TreeType X);
+addrNode AlokNode(infotype X)
 /* Mengirimkan addrNode hasil alokasi sebuah elemen */
 /* Jika alokasi berhasil, maka addrNode tidak Nil, dan misalnya menghasilkan P, 
   maka Akar(P) = X, Left(P) = Nil, Right(P)=Nil */
 /* Jika alokasi gagal, mengirimkan Nil */
+{
+  addrNode P;
 
-void DealokNode(addrNode P);
+  P = (addrNode)malloc(sizeof(infotype));
+  if (P != Nil){
+    Akar(P) = X;
+    Right(P) = Nil;
+    Left(P) = Nil;
+  }
+  return P;
+}
+void DealokNode(addrNode P)
 /* I.S. P terdefinisi */
 /* F.S. P dikembalikan ke sistem */
 /* Melakukan dealokasi/pengembalian addrNode P */
-
+{
+  free(P);
+}
 /* *** Predikat-Predikat Penting *** */
-
-boolean IsTreeEmpty(BinTree P);
+/* *** Predikat-Predikat Penting *** */
+boolean IsTreeEmpty(BinTree P)
 /* Mengirimkan true jika P adalah pohon biner kosong */
-
-boolean IsTreeOneElmt(BinTree P);
+{
+  return (P == Nil);
+}
+boolean IsTreeOneElmt(BinTree P)
 /* Mengirimkan true jika P adalah pohon biner tidak kosong dan hanya memiliki 1 elemen */
-
-boolean IsUnerLeft(BinTree P);
+{
+  if (!IsTreeEmpty(P)){
+    return ((Left(P) == Nil) && Right(P) == Nil);
+  }
+}
+boolean IsUnerLeft(BinTree P)
 /* Mengirimkan true jika pohon biner tidak kosong P adalah pohon unerleft: hanya mempunyai subpohon kiri */
-
-boolean IsUnerRight(BinTree P);
+{
+  return ((Right(P) == Nil) && (Left(P) != Nil));
+}
+boolean IsUnerRight(BinTree P)
 /* Mengirimkan true jika pohon biner tidak kosong P adalah pohon unerright: hanya mempunyai subpohon kanan*/
+{
+  return ((Right(P) != Nil) && (Left(P) == Nil));
+}
 
-void PrintTree(BinTree P, int h);
+void PrintTree(BinTree P, int h)
 /* I.S. P terdefinisi, h adalah jarak indentasi (spasi) */
 /* F.S. Semua simpul P sudah ditulis dengan indentasi (spasi) */
 /* Penulisan akar selalu pada baris baru (diakhiri newline) */
@@ -86,14 +108,40 @@ A
   C
     E
 */
+{
+  //Not Yet
+}
 
 /* *** Searching *** */
-boolean SearchTree(BinTree P, TreeType X);
+boolean SearchTree(BinTree P, infotype X)
 /* Mengirimkan true jika ada node dari P yang bernilai X */
-
+{
+  if (P == Nil){
+    return false;
+  } else {
+    if (X == Akar(P)){
+      return true;
+    } else {
+      return (Search(Left(P),X) || Search(Right(P),X));
+    }
+  }
+}
 /* *** Fungsi-Fungsi Lain *** */
-int Level(BinTree P, TreeType X);
+int Level(BinTree P, infotype X)
 /* Mengirimkan level dari node X yang merupakan salah satu simpul dari pohon biner P. 
    Akar(P) level-nya adalah 1. Pohon P tidak kosong. */
-
-#endif
+{
+  //Kamus Lokal
+	
+	//Algoritma
+	if (X==Akar(P)){
+		return 1;
+	}
+	else 
+		if (SearchPB(Left(P), X)) {
+			return 1+Level(Left(P), X);
+		}
+		else {
+			return 1+Level(Right(P), X);
+		}
+}
