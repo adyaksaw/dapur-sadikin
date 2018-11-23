951475823d68aa13ab08989898b0c0fb0be28b3e
Firastic <adyaksa.wisanggeni@gmail.com>
2018-11-23 17:09:56 +0700

    Create graph.c

---
+++ lib/data_structure/adt/graph.c
@@ -0,0 +1,37 @@
+#include "graph.h"
+
+void CreateGraph (int X , Graph *L){
+
+}
+
+adrNode AlokNode (int X){
+
+}
+
+void DealokNode (adrNode P){
+
+}
+
+adrSuccNode AlokSuccNode(adrNode Pn){
+
+}
+
+void DealokSuccNode(adrSuccNode P){
+
+}
+
+adrNode SearchNode (Graph G , int X){
+
+}
+
+adrSuccNode SearchEdge(Graph G, int prec, int succ){
+
+}
+
+void InsertNode (Graph *G, int X , adrNode *Pn){
+
+}
+
+void InsertEdge (Graph *G, int prec, int succ){
+	
+}
